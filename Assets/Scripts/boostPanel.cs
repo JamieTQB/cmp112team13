773541d8298a313ebe971a11be9936f9c70b89f9
JamieTQB <maccallumjamie@gmail.com>
2025-10-29 19:02:21 +0000

    added boost panels

---
+++ Assets/Scripts/boostPanel.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class boostPanel : MonoBehaviour
+{
+    public GameObject player1;
+    //public GameObject player2;
+    public float boostValue = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void onTriggerEnter(Collider player)
+    {
+        if (player == player1)
+        {
+            player1
+        else if (player == player2)
+            {
+
+            }
+        }
+    }
+}
--- Assets/Scripts/goalScore.cs
+++ Assets/Scripts/goalScore.cs
@@ -4,10 +4,11 @@ public class Score : MonoBehaviour
 {
     public GameObject Ball;
     public GameObject Player1;
-    //public GameObject Player2;
+    //public GameObject Player2; <- temp commented out until second player object is added
     Vector3 ballSpawn;
     Vector3 player1Spawn;
-    //Vector3 player2Spawn;
+    //Vector3 player2Spawn; <- temp commented out until second player object is added
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,15 +24,18 @@ public class Score : MonoBehaviour
 
     private void OnTriggerExit(Collider collider)
     {
-        ballSpawn = new Vector3(0f, 1f, 0f);
-        player1Spawn = new Vector3(0f, 0.6f, -35f);
-        //player2Spawn = new Vector3(x y z)
-        if (collider.gameObject == Ball)
+        ballSpawn = new Vector3(0f, 1f, 0f);            //sets the point in the level for the ball to spawn
+        player1Spawn = new Vector3(0f, 0.6f, -35f);     //does the same as above but for player 1 spawn
+        //player2Spawn = new Vector3(x y z)             //same as above but for player 2, temp commented until second player object is added
+
+        //if loop checks for if the colliding obect is the ball and then resets the player(s) and ball
+        if (collider.gameObject == Ball)    
         {
             Debug.Log($"GOAL!");
             Ball.transform.position = ballSpawn;
             Player1.transform.position = player1Spawn;
+            //player2.transform.position = player2Spawn; <- temp commented out until second player object is added
         }
-        
+
     }
 }
