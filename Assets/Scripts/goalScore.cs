35159758bdc10a5f1d11a025d9d60315487569e1
JamieTQB <maccallumjamie@gmail.com>
2025-10-31 11:54:20 +0000

    added ui, tweaked some physics related stuff
    Added a basic UI displaying the score of the game, tweaked some physics properties of the ball, added physic materials for the floor and ball.

--- Assets/Scripts/goalScore.cs
+++ Assets/Scripts/goalScore.cs
@@ -1,18 +1,26 @@
+using System.Collections;
 using UnityEngine;
+using TMPro;
+using System.Net.Http.Headers;
 
 public class Score : MonoBehaviour
 {
+    private Rigidbody rb;
+    public TextMeshProUGUI scoreboard;
     public GameObject Ball;
     public GameObject Player1;
     //public GameObject Player2; <- temp commented out until second player object is added
     Vector3 ballSpawn;
     Vector3 player1Spawn;
     //Vector3 player2Spawn; <- temp commented out until second player object is added
+    float player1Score = 0;
+    //float player2Score = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SetScoreboard();
+        rb = Ball.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -28,14 +36,30 @@ public class Score : MonoBehaviour
         player1Spawn = new Vector3(0f, 0.6f, -35f);     //does the same as above but for player 1 spawn
         //player2Spawn = new Vector3(x y z)             //same as above but for player 2, temp commented until second player object is added
 
-        //if loop checks for if the colliding obect is the ball and then resets the player(s) and ball
+        //if loop checks if the colliding obect is the ball and then resets the player(s) and ball
         if (collider.gameObject == Ball)    
         {
             Debug.Log($"GOAL!");
-            Ball.transform.position = ballSpawn;
-            Player1.transform.position = player1Spawn;
-            //player2.transform.position = player2Spawn; <- temp commented out until second player object is added
-        }
+            player1Score = player1Score + 1;
+            SetScoreboard();
 
+            StartCoroutine(Respawn());
+        }
+    }
+
+    void SetScoreboard()
+    {
+        scoreboard.text = player1Score + " - P1 Score P2 - 0";    // + player2Score
+    }
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("waited");
+        Ball.transform.position = ballSpawn;
+        rb.angularVelocity.Set(0, 0, 0);
+        rb.linearVelocity.Set(0, 0, 0);
+        Player1.transform.position = player1Spawn;
+        Player1.transform.rotation = new Quaternion(0, 0, 0, 0);
+        //player2.transform.position = player2Spawn; <- temp commented out until second player object is added
     }
 }
