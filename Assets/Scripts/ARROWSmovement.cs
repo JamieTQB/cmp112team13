490c779e58990972245a3818e25c0ff19ed430c3
rhianhorne07 <rhianhorne1w@gmail.com>
2025-11-07 15:19:10 +0000

    Added Split Screen
    Reverted to old input system but its not functional, working on it.

---
+++ Assets/Scripts/ARROWSmovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ARROWSmovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move += Vector3.left;
+        }
+
+        move = move.normalized * speed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+    }
+}
